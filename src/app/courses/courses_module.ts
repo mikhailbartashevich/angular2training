452ef8d59a287375c559220960dc6ebddbfdaf53
Mikhail Bartashevich <mihailbartashevich@gmail.com>
2017-11-21 17:37:00 +0100

    modules

--- src/app/courses/courses_module.ts
+++ src/app/courses/courses_module.ts
@@ -2,12 +2,13 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 
+import {SharedModule} from '../shared/shared_module';
+import {CoreModule} from '../core/core_module';
+
 import {CoursesPageComponent} from './courses_page_component';
 import {CourseListComponent} from './course_list_component/course_list_component';
 import {CourseItemComponent} from './course_item_component/course_item_component';
 import {CoursesToolboxComponent} from './courses_toolbox_component/courses_toolbox_component';
-import {SharedModule} from '../shared/shared_module';
-import {CoreModule} from '../core/core_module';
 
 @NgModule({
   imports: [FormsModule, CoreModule, CommonModule, SharedModule],
